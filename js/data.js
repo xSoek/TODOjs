72d50a6d11ae57f515e945e50d08482680e71211
Jorge de Paz <jorge.depazcabanas@gmail.com>
2022-06-02 00:04:06 +0200

    Testing data added

---
+++ js/data.js
@@ -0,0 +1,43 @@
+let tasks = {
+    arrToDo: [
+        {
+            _id: 2,
+            title: "Hacer algo",
+            priority: "high",
+        },
+
+        {
+            _id: 1,
+            title: "Hacer algo 1",
+            priority: "medium",
+        },
+
+        {
+            _id: 4,
+            title: "Hacer algo 2",
+            priority: "low",
+        },
+    ],
+    arrWIP: [
+        {
+            _id: 3,
+            title: "Hacer algo",
+            priority: "high",
+        },
+
+        {
+            _id: 6,
+            title: "Hacer algo 1",
+            priority: "medium",
+        },
+    ],
+    arrReview: [
+        {
+            _id: 5,
+            title: "Hacer algo",
+            priority: "high",
+        },
+    ],
+    arrDone: [],
+
+}
