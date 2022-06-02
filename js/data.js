a0baa6fbccc6021d488b19c3139c910b5d02de93
Jorge de Paz <jorge.depazcabanas@gmail.com>
2022-06-03 01:07:22 +0200

    Changed data for testing

--- js/data.js
+++ js/data.js
@@ -2,39 +2,39 @@ let tasks = {
     arrToDo: [
         {
             _id: 2,
-            title: "Hacer algo",
+            title: "Hacer",
             priority: "high",
         },
 
         {
             _id: 1,
-            title: "Hacer algo 1",
+            title: "Comer algo 1",
             priority: "medium",
         },
 
         {
             _id: 4,
-            title: "Hacer algo 2",
+            title: "Saltar",
             priority: "low",
         },
     ],
     arrWIP: [
         {
             _id: 3,
-            title: "Hacer algo",
+            title: "Hacer",
             priority: "high",
         },
 
         {
             _id: 6,
-            title: "Hacer algo 1",
+            title: "Fregar",
             priority: "medium",
         },
     ],
     arrReview: [
         {
             _id: 5,
-            title: "Hacer algo",
+            title: "Cocinar",
             priority: "high",
         },
     ],
